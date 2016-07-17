7bbb4f206a86cf44df7ec575da7724d58270d5f0
Ubuntu <ubuntu@ip-172-31-19-41.ap-south-1.compute.internal>
2016-07-17 12:47:35 +0000

    mysql aws

--- share/share/settings.py
+++ share/share/settings.py
@@ -83,8 +83,16 @@ WSGI_APPLICATION = 'share.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+	'NAME': 'putboard_db',
+        'USER': 'root',
+        'PASSWORD': 'putboard@165',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
+	#'OPTIONS': {
+        #    'read_default_file': '/etc/mysql/my.cnf',
+        #},
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
