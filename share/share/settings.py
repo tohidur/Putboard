45029b0a666eff77ee69fffe3341e86c1ebbe87f
Ubuntu <ubuntu@ip-172-31-19-41.ap-south-1.compute.internal>
2016-07-17 19:58:35 +0000

    Fixed check of static and media cdn

--- share/share/settings.py
+++ share/share/settings.py
@@ -78,8 +78,8 @@ TEMPLATES = [
 WSGI_APPLICATION = 'share.wsgi.application'
 
 
-Database
-https://docs.djangoproject.com/en/1.9/ref/settings/#databases
+#Database
+#https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
