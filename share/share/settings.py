5f6a076e5b0b31af81867d211a9dca390894123b
Tohidur Rahaman <rahaman.rjh@gmail.com>
2016-07-23 04:04:06 +0530

    banck ot rahaman.rjh

--- share/share/settings.py
+++ share/share/settings.py
@@ -11,12 +11,12 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = 'tohidur920@gmail.com'
-SERVER_EMAIL = 'tohidur920@gmail.com'
+DEFAULT_FROM_EMAIL = 'rahaman.rjh@gmail.com'
+SERVER_EMAIL = 'rahaman.rjh@gmail.com'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'tohidur920@gmail.com'
-EMAIL_HOST_PASSWORD = 'rahaman1653.1415926'
+EMAIL_HOST_USER = 'rahaman.rjh@gmail.com'
+EMAIL_HOST_PASSWORD = 'REhmanRJH165@.'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 import os
@@ -32,7 +32,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'qmgsjezrf+2d7kxglcel$bamc&fz111)9k%&e5#g&t_6$t)r4a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
