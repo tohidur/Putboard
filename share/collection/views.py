c8976ce0f68216bd2dc82cccb629664f9af84985
Tohidur Rahaman <rahaman.rjh@gmail.com>
2016-07-27 14:04:59 +0530

    tags problem

--- share/collection/views.py
+++ share/collection/views.py
@@ -137,6 +137,7 @@ def link_add(request, slug=None):
         
         tags = request.POST.getlist('tags[]')
         for tag in tags:
+            tag.replace(" ", "_")
             x, created = Tag.objects.get_or_create(name = tag)
             instance.tags.add(x)
         
