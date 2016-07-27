249fb3ed2840eec3bed078958917b8e4f58f570d
Tohidur Rahaman <rahaman.rjh@gmail.com>
2016-07-27 20:48:31 +0530

    same

--- share/collection/views.py
+++ share/collection/views.py
@@ -139,8 +139,9 @@ def link_add(request, slug=None):
         for tag in tags:
             tag = tag.replace(" ", "_")
             print tag
-            tag = get_object_or_404(Tag, id=tag)
-            print tag
+            if(get_object_or_404(Tag, id=tag)):
+                print get_object_or_404(Tag, id=tag)
+            # print tag
             x, created = Tag.objects.get_or_create(name = tag)
             instance.tags.add(x)
         
